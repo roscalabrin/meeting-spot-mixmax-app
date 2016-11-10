acfed90ebcd335e6890293b0a84ed937e5095474
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-10 14:16:34 -0700

    initial setup

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require ('express')
+// const bodyParser = require('body-parser')
+const app = express()
+// const cors = require('cors')
+
+app.set('port', process.env.PORT || 8910)
+
+// Serve assets in /public.
+// app.use(express.static(__dirname + '/public'))
+
+// So we can POST.
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }))
+
+// Since Mixmax calls this API directly from the client-side, it must be whitelisted.
+// const corsOptions = {
+//   origin: /^[^.\s]+\.mixmax\.com$/,
+//   credentials: true
+// }
+
+// The editor interface.
+// app.get('/editor', (request, response) => {
+//   response.sendFile(path.join(__dirname + '/editor.html'))
+// })
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+// The in-email representation.
+// app.post('/api/resolver', cors(corsOptions), require('./api/resolver'))
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
