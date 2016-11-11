446a6b034a9a9cce402acb31fe9cd4315eeb2c49
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-11 10:51:27 -0700

    add functionality to store coordinates for addresses in local storage

--- public/lib/index.js
+++ public/lib/index.js
@@ -11,19 +11,39 @@ class Location {
     $('.search-locations').on('click', (e) => {
       const location1 = $('.location1-search-field').val()
       const location2 = $('.location2-search-field').val()
-      this.findCoord(location1, location2)
+      const location1Formatted = this.formatAddress(location1)
+      const location2Formatted = this.formatAddress(location2)
+      this.findCoord("location1", location1Formatted)
+      this.findCoord("location2", location2Formatted)
+      this.calculateDistance()
     })
   }
   
-  findCoord(location1, location2) {
-    debugger
+  formatAddress(address) {
+    const array = address.split(' ')
+    const alteredArray = []
+    for (var i = 0; i < array.length; i++) {
+      alteredArray.push(array[i] + '+')
+    }
+    return alteredArray
+  }
+  
+  findCoord(place, locationFormatted) {
+  
     $.ajax({
       type: "GET",
-      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3719+West26th+Ave+Denver+CO&key=',
-      success: function(response) {
-        var coord = response.results[0].geometry.location
-        console.log(coord)
-      }
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${locationFormatted}&key=${apiKey}`,
+      success: data => success(place, data)
     })
+    function success(place, data) {
+      const response = data.results[0].geometry.location
+      const coords = `lat:${response.lat}, lng:${response.lng}`
+      localStorage.setItem(place, coords)
+    }
   }
+  
+  calculateDistance() {
+    
+  }
+  
 }
