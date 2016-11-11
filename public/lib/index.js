106dcbf276f5f8a8619d2df7d3cbc51b8007d0cd
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-11 11:10:20 -0700

    add function to calculate mid point

--- public/lib/index.js
+++ public/lib/index.js
@@ -15,7 +15,7 @@ class Location {
       const location2Formatted = this.formatAddress(location2)
       this.findCoord("location1", location1Formatted)
       this.findCoord("location2", location2Formatted)
-      this.calculateDistance()
+      this.midPoint()
     })
   }
   
@@ -42,8 +42,30 @@ class Location {
     }
   }
   
-  calculateDistance() {
-    
+  midPoint() {
+    // var midPoint = function(latitude1, longitude1, latitude2, longitude2) {
+    const DEG_TO_RAD = Math.PI / 180;     // To convert degrees to radians.
+    const latitude1 = localStorage.getItem('location1').split(',')[0].split(':')[1]
+    const latitude2 = localStorage.getItem('location2').split(',')[0].split(':')[1]
+    const longitude1 = localStorage.getItem('location1').split(',')[1].split(':')[1]
+    const longitude2 = localStorage.getItem('location2').split(',')[1].split(':')[1]
+    // Convert latitude and longitudes to radians:
+    const lat1 = latitude1 * DEG_TO_RAD;
+    const lat2 = latitude2 * DEG_TO_RAD;
+    const lng1 = longitude1 * DEG_TO_RAD;
+    const dLng = (longitude2 - longitude1) * DEG_TO_RAD;  // Diff in longtitude.
+ 
+    // Calculate mid-point:
+    const bx = Math.cos(lat2) * Math.cos(dLng);
+    const by = Math.cos(lat2) * Math.sin(dLng);
+    const lat = Math.atan2(
+        Math.sin(lat1) + Math.sin(lat2),
+        Math.sqrt((Math.cos(lat1) + bx) * (Math.cos(lat1) + bx) + by * by));
+    const lng = lng1 + Math.atan2(by, Math.cos(lat1) + bx);
+    debugger
+    // lat / DEG_TO_RAD
+    // lng / DEG_TO_RAD
+    const array = []
   }
   
 }
