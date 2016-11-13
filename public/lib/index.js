4f351c7356804ac032607c49bbc6197b5467014a
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-13 00:20:37 -0700

    search functionality working need to refactor

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,19 +1,7 @@
 $(document).ready( () => {
   localStorage.setItem('location1', '')
   localStorage.setItem('location2', '')
-  localStorage.setItem('center', '')
-  // autocomplete.addListener('place_changed', function() {
-  //   debugger
-  //   // infowindow.close();
-  //   // marker.setVisible(false);
-  //   // var place = autocomplete.getPlace();
-  //   // if (!place.geometry) {
-  //   //   // User entered the name of a Place that was not suggested and
-  //   //   // pressed the Enter key, or the Place Details request failed.
-  //   //   window.alert("No details available for input: '" + place.name + "'");
-  //   //   return;
-  //   }
-  // }
+  localStorage.setItem('center', '')  
   // $('.search-locations').on('click', (e = '') => {
   //   google.maps.event.trigger(markers[2], 'click');
   //   const location1 = $('.location1-search-field').val()
